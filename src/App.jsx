f4a796307f406847cb1f0a1bfbfdf5797809017e
GAKU <gakugaku1114@iCloud.com>
2023-10-10 15:16:40 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,25 @@ import React, { useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(true);
   const onClickCountUp = () => {
     setNum(num + 1);
   };
-  const [num, setNum] = useState(0);
-
-  const contentLedyStyle = {
-    color: "pink",
-    fontSize: "18px",
+  const onClickSwitchShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
   };
+
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは!</h1>
       <ColorfulMessage color="blue" message="お元気ですか？" />
       <ColorfulMessage color="pink" message="元気です!" />
       <button onClick={onClickCountUp}>カウントアップ!</button>
-      <p>( ͡° ͜ʖ ͡°)</p>
+      <br />
+      <button onClick={onClickSwitchShowFlag}>on/off</button>
+      <p>{num}</p>
+      {faceShowFlag && <p>( ͡° ͜ʖ ͡°)</p>}
     </>
   );
 };
